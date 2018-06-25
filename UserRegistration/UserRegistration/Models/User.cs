313e811816670571cc4112f67c08e54081276c2f
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-06-25 22:00:00 +0530

    Confirm Password
    Added confirm password prop and text box field.

--- UserRegistration/UserRegistration/Models/User.cs
+++ UserRegistration/UserRegistration/Models/User.cs
@@ -11,12 +11,15 @@ namespace UserRegistration.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+
     public partial class User
     {
         public int UserID { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        [DisplayName("Confirm Password")]
+        public string ConfirmPassword { get; set; }
         public Nullable<bool> IsAdmin { get; set; }
     }
 }
