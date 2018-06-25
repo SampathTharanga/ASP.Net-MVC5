6f9b115b765ed7ed35b91bacf7ce0785da294f9b
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-06-25 22:05:56 +0530

    Password Type
    Password and Confirm password field for set the Password type.

--- UserRegistration/UserRegistration/Models/User.cs
+++ UserRegistration/UserRegistration/Models/User.cs
@@ -12,12 +12,16 @@ namespace UserRegistration.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
 
     public partial class User
     {
         public int UserID { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string UserName { get; set; }
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        [DataType(DataType.Password)]
         [DisplayName("Confirm Password")]
         public string ConfirmPassword { get; set; }
         public Nullable<bool> IsAdmin { get; set; }
